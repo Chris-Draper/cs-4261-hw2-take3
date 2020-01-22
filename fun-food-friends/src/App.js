778c158057770c57a12b19dea02aced5ad610549
Matthew Wang <mwang7313@gmail.com>
2020-01-22 01:29:59 -0500

    form now adds information into Firebase

--- fun-food-friends/src/App.js
+++ fun-food-friends/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js';
 
 class App extends Component {
   // app constructor
@@ -11,11 +12,25 @@ class App extends Component {
       username: ''
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   // handle any changes in the app
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username
+    }
+    itemsRef.push(item);
+    this.setState({
+      currentItem: '',
+      username: ''
     });
   }
   // render app component
@@ -30,13 +45,11 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className='add-item'>
-              <form>
-                <input type="text" name="username" placeholder="What's your name?" 
-                  onChange={this.handleChange} value={this.state.username}/>
-                <input type="text" name="currentItem" placeholder="What are you bringing?" 
-                  onChange={this.handleChange} value={this.state.currentItem}/>
-                <button>Add Item</button>
-              </form>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+            <input type="text" name="currentItem" placeholder="What are you bringing ?" onChange={this.handleChange} value={this.state.currentItem} />
+            <button>Add Item</button>
+          </form>
           </section>
           <section className='display-item'>
             <div className='wrapper'>
